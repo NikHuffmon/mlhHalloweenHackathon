ddb82a9f2721b149ce62495206697233092e3cfa
DragonCraft618 <ZITKOA@cbchs.org>
2021-10-29 13:07:37 -0500

    added frame background color, main code, and size of window

---
+++ candy_page.py
@@ -0,0 +1,10 @@
+#gui to sort candy
+import tkinter as tk
+win = tk.Tk()
+win.geometry('450x400')
+win.configure(background='#e86c3f')
+
+candy_Frame = tk.Frame(win, background = '#e86c3f')
+
+
+win.mainloop()
