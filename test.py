39c6d50a0b8881a4fa53a423c36b86d83517b257
Panda8324 <nortonc@cbchs.org>
2021-10-29 13:09:24 -0500

    worked on home page

---
+++ test.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.configure(background="#e89c3f")
+win.geometry("500x500")
+
+def labelChanage():
+    finalChange = change.get()
+    welcome_entry.configure(win, text=f"{finalChange}")
+welcome_frame = tk.Frame(win, )
+welcome_frame.grid(row=0, column=0)
+welcomeLaBEL = tk.Label(welcome_frame, text=" Welocome to CSA wich stands for The Candy Sorter App ", foreground="#000", background="#e89c3f", font=" Georiga 14")
+welcomeLaBEL.grid(row= 0, column=0)
+
+entry_frame = tk.Frame(win, )
+entry_frame.grid(row=1, column=0)
+change = tk.StringVar
+welcome_entry = tk.Entry(entry_frame, text="", textvariable=change)
+welcome_entry.grid(row= 1, column=1)
+
+win.mainloop()
