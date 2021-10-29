ca1e5733d3693644f41ab9f73ba4b412f193cb8a
Hallerc2004 <hallerc@cbchs.org>
2021-10-29 14:09:09 -0500

    deleted stuff and comment out photos

--- README.md
+++
@@ -1,3 +0,0 @@
-# halloweenTryTwo
-hi
-hello
--- candy_page.py
+++ candy_page.py
@@ -7,15 +7,19 @@ win.configure(background='#e86c3f')
 #function for the button picture
 
 #adding photos of candy
-
-
+#candycorn_Photo = tk.PhotoImage(files = '')
+#twix_Photo = tk.PhotoImage(files = '')
+#mily_way_Photo = tk.PhotoImage(files = '')
 candylist = []
 index = 0
 
 candy_Frame = tk.Frame(win, background = '#e86c3f')
 candy_Frame.pack()
 
-candy_name_label = tk.Label(candy_Frame, text='hi', font = 'Times 12', foreground = '#000')
+candy_name_label = tk.Label(candy_Frame, text='', font = 'Times 12')
 candy_name_label.grid(column=1,row =0)
+#nutrition area
+
+#frame for the list
 
 win.mainloop()
