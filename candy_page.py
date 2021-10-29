d3c489f1de209e7514e8eda7f29c89d17cc18e47
Hallerc2004 <hallerc@cbchs.org>
2021-10-29 13:11:14 -0500

    added a mian window, frame to house the candy area, then place the fram on the grid at 0,0

--- candy_page.py
+++ candy_page.py
@@ -3,7 +3,9 @@ import tkinter as tk
 win =tk.Tk()
 win.geometry('450x400')
 win.configure(background='#e86c3f')
-candy_Frame = tk.Frame(win, background = '#e86c3f')
 
+
+candy_Frame = tk.Frame(win, background = '#e86c3f')
+candy_Frame.grid(columm =0, row =0)
 
 win.mainloop()
