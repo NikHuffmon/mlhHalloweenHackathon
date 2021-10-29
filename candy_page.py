f40273e0f085133f76683fadc5620b3f0c8f0835
DragonCraft618 <ZITKOA@cbchs.org>
2021-10-29 13:46:06 -0500

    added candy_name_label

--- candy_page.py
+++ candy_page.py
@@ -1,10 +1,19 @@
 #gui to sort candy
 import tkinter as tk
+from tkinter import font
 win = tk.Tk()
 win.geometry('450x400')
-win.configure(background='#e86c3f')
+win.configure(background ='#e86c3f')
+
+#function for the button picture
+#
+candy_List=[]
+index=0
 
 candy_Frame = tk.Frame(win, background = '#e86c3f')
+candy_Frame.pack()
 
+candy_name_label = tk.Label(candy_Frame , text='Hi' , font='Times 12' , background = '#e86c3f')
+candy_name_label.grid(column=0 , row=0)
 
 win.mainloop()
